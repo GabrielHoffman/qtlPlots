366c86b55d1c69f1a2ac9701a8b1b77a2279602c
Gabriel Hoffman <gabriel.hoffman@gmail.com>
2023-02-15 15:11:34 -0500

    add ymax

--- R/plotPosterior.R
+++ R/plotPosterior.R
@@ -10,11 +10,12 @@
 #' @param grObj GRanges object with scores as the posterior inclusion probability
 #' @param wh genome interval to plot
 #' @param size font size
+#' @param ymax max value on y axis
 #'
 #' @import ggplot2 GenomicRanges 
 #' @importFrom scales comma
 #' @export
-plotPosterior = function( grObj, wh, size=8 ){
+plotPosterior = function( grObj, wh, size=8, ymax=1 ){
 
 	# extact scores *within* the region wh
     gr_sub = grObj[grObj %within% wh]
@@ -22,5 +23,5 @@ plotPosterior = function( grObj, wh, size=8 ){
 
 	fig = ggplot(df_sub, aes(Position, score)) + theme_classic(size)
 
-	fig + geom_point(size=.4, color="red") + ylab("Posterior") + scale_y_continuous(expand=c(0,0.01), limits=c(0, 1)) + scale_x_continuous(expand=c(0,0), limits=c(start(wh), end(wh)), label=comma) + theme(legend.position="none") 
+	fig + geom_point(size=.4, color="red") + ylab("Posterior") + scale_y_continuous(expand=c(0,0.01), limits=c(0, ymax)) + scale_x_continuous(expand=c(0,0), limits=c(start(wh), end(wh)), label=comma) + theme(legend.position="none") 
 }	
